5a10ee4e7f198c60a48699e5f67acd2849ac4501
Rajdev Kapoor <rajdev1409@gmail.com>
2020-07-07 05:27:21 +0530

    Contact App Backened ->CRUD operations ->getCount

---
+++ app/src/main/java/com/rajdevkapoor/contactsapp/MainActivity.java
@@ -0,0 +1,26 @@
+package com.rajdevkapoor.contactsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.rajdevkapoor.contactsapp.data.DatabaseHandler;
+import com.rajdevkapoor.contactsapp.model.Contact;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        DatabaseHandler db= new DatabaseHandler(this);
+        List<Contact> contactList=db.getAllContacts();
+        for(Contact contact: contactList){
+            Log.d("Mainactivity",contact.getName()+" "+contact.getPhoneNumber());
+        }
+
+    }
+}
---
+++ app/src/main/java/com/rajdevkapoor/contactsapp/util/Util.java
@@ -0,0 +1,12 @@
+package com.rajdevkapoor.contactsapp.util;
+
+public class Util {
+    public static final int DATABASE_VERSION =1;
+    public static final String DATABASE_NAME =  "contact_db";
+    public static final String TABLE_NAME = "contacts";
+
+    //Column names
+    public static final String KEY_ID = "ID";
+    public static final String KEY_NAME = "name";
+    public static final String KEY_PHONE_NUMBER = "phone_number";
+}
